593bb531447d63e924a6b4f649552d2b9c3de447
Avantika tiwari <avantikatiwari18@gmail.com>
2023-07-14 13:25:57 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Task from "./Components/Task";
+// import We from "./Components/We";
+
 // import Thedatabase from "./Components/Thedatabase";
 
 function App() {
--- src/Components/Task.js
+++ src/Components/Task.js
@@ -1,36 +1,58 @@
 import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Task = () => {
-  const [newtask, settask] = useState([]);
-  const [newtasktitle, setnewtasktitle] = useState("");
-  const [newtaskdescription, setnewtaskdescription] = useState("");
-  const [newtaskduedate, setnewtaskduedate] = useState([new Date()]);
+  const [tasks, setasks] = useState([]);
+  const [taskTitle, setTaskTitle] = useState();
+  const [taskDescription, settaskDescription] = useState();
+  const [taskDueDate, settaskDueDate] = useState();
   const createTask = () => {
-    const oldtask = [...newtask];
-    const Uptask = {
-      taskname: newtasktitle,
-      taskdescription: newtaskdescription,
-      taskdate: newtaskduedate,
-      taskstatus: "To Do",
+    if (taskTitle === "") return toast.warning("Please provide task title");
+    else if (taskDescription === "")
+      return toast.warning("Please enter task description");
+    else if (!taskDueDate) return toast.warning("Please provide due date");
+    const newTask = {
+      tasktitle: taskTitle,
+      taskdescription: taskDescription,
+      taskduedate: taskDueDate,
+      taskstatus: "TO DO",
     };
-    oldtask.push(Uptask);
-    settask(oldtask);
+    const oldTask = [...tasks];
+    oldTask.push(newTask);
+    setasks(oldTask);
+    setTaskTitle("");
+    settaskDescription("");
   };
-
-  const updatedtask = (c) => {
-    const oldtask = [...newtask];
-    oldtask[c].taskstatus = "In Progress";
-
-    settask(oldtask);
+  const handlechangestatus = (index, status) => {
+    let oldTask = [...tasks];
+    if (status === "TO DO") {
+      oldTask[index].taskstatus = "ON GOING";
+    } else if (status === "ON GOING") {
+      oldTask[index].taskstatus = "COMPLETED";
+    } else if (status === "COMPLETED") {
+      oldTask[index].taskstatus = "TO DO";
+    }
+    setasks(oldTask);
   };
-  const deletetask = (c) => {
-    let oldtask = [...newtask];
-    const uptask = oldtask.filter((v, i) => i !== c);
-    settask(uptask);
+  const handledeletetask = (index) => {
+    let oldTask = [...tasks];
+    let updatedTask = oldTask.filter((v, i) => i !== index);
+    setasks(updatedTask);
   };
-
+  const getButtonName = (status) => {
+    if (status === "TO DO") return "START TASK";
+    else if (status === "ON GOING") return "COMPLETE TASK";
+    else if (status === "COMPLETED") return "RESTART";
+  };
+  const getColor = (status) => {
+    if (status === "TO DO") return "darkblue";
+    else if (status === "ON GOING") return "darkgreen";
+    else if (status === "COMPLETED") return "orange";
+  };
   return (
     <>
+      <ToastContainer />
       <div
         style={{
           alignItems: "center",
@@ -52,89 +74,91 @@ const Task = () => {
           position: "relative",
         }}
       >
-        <h1 style={{ backgroundColor: "", color: "black" }}>
-          Task Management Dashboard
-        </h1>
-        <div className="mt-2;">
-          <label>Task Title </label>
-          <input
-            value={newtasktitle}
-            onChange={(e) => setnewtasktitle(e.target.value)}
-            type="text"
-            placeholder="Enter your task"
-          />
-        </div>
-        <br />
-        <div className="mt-2;">
-          <label>Task Description</label>
-          <textarea
-            value={newtaskdescription}
-            onChange={(e) => setnewtaskdescription(e.target.value)}
-            textareatype="text"
-            placeholder="Enter Description"
-          />
-        </div>
-        <br />
-        <div className="mt-2;">
-          <label>Task Due Date</label>
-          <input
-            value={newtaskduedate}
-            onChange={(e) => setnewtaskduedate(e.target.value)}
-            type="date"
-          />
-        </div>
-        <br />
-        <button
-          class="createtask"
-          style={{ backgroundColor: "cornsilk" }}
-          onClick={() => createTask()}
-        >
-          Create Task
-          {/* <AddBoxIcon /> */}
-        </button>
+        <h1> Task Management Dashboard</h1>
+        <>
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail">Task Title</label>
+              {/* Label tag is used to specify a label for an <input>element of a form */}
+              <input
+                type="text"
+                required
+                value={taskTitle}
+                onChange={(e) => setTaskTitle(e.target.value)}
+                class="form-control"
+                id="exampleInputEmail"
+                aria-describedby="emailHelp"
+                placeholder="Enter Task Title"
+              />
+            </div>
+
+            <div class="form-group">
+              <label for="exampleInputPassword1">Task Description</label>
+              <textarea
+                required
+                value={taskDescription}
+                onChange={(e) => settaskDescription(e.target.value)}
+                type="text"
+                class="form-control"
+                id="exampleInputPassword1"
+                placeholder="Enter Task Description"
+              />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Task Due Date</label>
+              <input
+                required
+                type="date"
+                value={taskDueDate}
+                onChange={(e) => settaskDueDate(e.target.value)}
+              />
+            </div>
+            <button
+              onClick={() => createTask()}
+              type="button"
+              class="btn btn-primary mt-2 p-2"
+            >
+              Create Task
+            </button>
+          </form>
+        </>
+        {tasks.map((v, i) => {
+          return (
+            <>
+              <div
+                class="card mt-2"
+                style={{
+                  backgroundColor: getColor(v.taskstatus),
+                  color: "yellow",
+                }}
+              >
+                <div class="card-body">
+                  <h5 class="card-title">{v.tasktitle}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">
+                    DUE: {v.taskduedate}
+                  </h6>
+                  <p class="card-text">{v.taskdescription}</p>
+                  <h7> {v.taskstatus}</h7>
+                  <button
+                    class="btn btn-primary me-2"
+                    onClick={() => handlechangestatus(i, v.taskstatus)}
+                  >
+                    {getButtonName(v.taskstatus)}
+                  </button>
+                  <button
+                    class="btn btn-secondary"
+                    onClick={() => handledeletetask(i)}
+                  >
+                    Delete Task
+                  </button>
+                </div>
+              </div>
+            </>
+          );
+        })}
       </div>
-      {newtask.map((v, i) => {
-        return (
-          <div
-            key={i}
-            className="m-2 p-2"
-            style={
-              v.taskstatus === "ON GOING"
-                ? { backgroundColor: "navy", color: "brown" }
-                : { backgroundColor: "coral", color: "red" }
-            }
-          >
-            <h3 style={{ backgroundColor: "black" }}> New Task</h3>
-            <ul>
-              <li>Title: {v.taskname}</li>
-              <li>Description:{v.taskdescription}</li>
-              <li>Date:{v.taskdate}</li>
-              <li>Status:{v.taskstatus}</li>
-            </ul>
-            <button
-              style={{
-                backgroundColor: "greenyellow",
-                color: "darkolivegreen",
-              }}
-              onClick={() => deletetask(i)}
-              type="button"
-            >
-              Delete
-            </button>
-            <button
-              style={{
-                backgroundColor: "greenyellow",
-                color: "darkolivegreen",
-              }}
-              onClick={() => updatedtask(i)}
-              type="button"
-            >
-              Start task
-            </button>
-          </div>
-        );
-      })}
     </>
   );
 };
+
 export default Task;
